7e8e67b7b750d55fe10270ad62ead0578300f35c
Pal Mezei <sirpalee@gmail.com>
2023-03-30 12:27:22 +1100

    Basic window.

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let _window = WindowBuilder::new()
+        .with_title("Basic Window")
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        if let Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } = event
+        {
+            *control_flow = ControlFlow::Exit;
+        }
+    });
+}
